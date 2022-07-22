c97e357e7b8f59beeb2ff141dc73a281d10d5230
Xwilarg <xwilarg@protonmail.com>
2022-07-23 00:42:26 +0200

    Add control point system

---
+++ Assets/Scripts/ControlPoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace TF2Jam
+{
+    public class ControlPoint : MonoBehaviour
+    {
+        [SerializeField]
+        private SpriteRenderer _pointSR, _iconSR;
+
+        [SerializeField]
+        private Sprite _pointRed, _iconRed;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                _pointSR.sprite = _pointRed;
+                _iconSR.sprite = _iconRed;
+            }
+        }
+    }
+}
