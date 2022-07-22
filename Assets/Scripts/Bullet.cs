b863ffaea71df97afa77fa8e01a5a88bf7fbf85b
Xwilarg <xwilarg@protonmail.com>
2022-07-22 21:48:52 +0200

    Improve physic

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -23,7 +23,7 @@ namespace TF2Jam
             var force = Vector2.Distance(contact, _owner.transform.position);
             if (force < _info.RocketImpactMaxDistance)
             {
-                var propForce = (force / _info.RocketImpactMaxDistance) * _info.RocketPropulsionForce;
+                var propForce = (1f - force / _info.RocketImpactMaxDistance) * _info.RocketPropulsionForce;
                 _owner.AddPropulsionForce(propForce, (Vector2)_owner.transform.position - contact);
             }
             Destroy(gameObject);
