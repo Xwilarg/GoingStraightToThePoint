3a41bb2b72e02f8c2a31e56ea76555f5897d86a1
Xwilarg <xwilarg@protonmail.com>
2022-07-24 19:00:43 +0200

    Add class switch in menu

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,12 +1,51 @@
 ﻿using TF2Jam.Persistency;
+using TF2Jam.Player;
 using TF2Jam.Translation;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace TF2Jam.Menu
 {
     public class MainMenu : MonoBehaviour
     {
+        [SerializeField]
+        private GameObject _modeToggle;
+
+        [SerializeField]
+        private Image _emblemDisplay;
+
+        [SerializeField]
+        private Sprite _emblemSoldier, _emblemDemoman;
+
+        [SerializeField]
+        private TMP_Text _currentClassText;
+
+        private void Awake()
+        {
+            if (!PersistencyManager.Instance.GetLevelData("3-3").IsHardModeUnlocked)
+            {
+                _modeToggle.gameObject.SetActive(false);
+            }
+            else
+            {
+                UpdateClassDisplay();
+            }
+        }
+
+        private void UpdateClassDisplay()
+        {
+            _emblemDisplay.sprite = PersistencyManager.Instance.CurrentClass == PlayerClass.Soldier ? _emblemSoldier : _emblemDemoman;
+            _currentClassText.text = PersistencyManager.Instance.CurrentClass == PlayerClass.Soldier ? "Soldier" : "Demoman";
+        }
+
+        public void NextClass()
+        {
+            PersistencyManager.Instance.CurrentClass = PersistencyManager.Instance.CurrentClass == PlayerClass.Soldier ? PlayerClass.Demoman : PlayerClass.Soldier;
+            UpdateClassDisplay();
+        }
+
         public void SetFrench() => SetLanguage("french");
 
         public void SetEnglish() => SetLanguage("english");
