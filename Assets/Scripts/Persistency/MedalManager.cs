05e4dc6cb70c79d2e8aefe2b0624f35508725717
Xwilarg <xwilarg@protonmail.com>
2022-07-24 16:15:05 +0200

    Improve medal management

--- Assets/Scripts/Persistency/MedalManager.cs
+++ Assets/Scripts/Persistency/MedalManager.cs
@@ -10,17 +10,17 @@ namespace TF2Jam.Persistency
             { "Tutorial1", (12f, 12f) },
             { "Tutorial2", (12f, 12f) },
             { "1-1", (4.5f, 5f) },
-            { "1-2", (5f, 5f) },
-            { "1-3", (3f, 3f) },
-            { "2-1", (7f, 8f) },
+            { "1-2", (4.5f, 4.5f) },
+            { "1-3", (2.5f, 2.5f) },
+            { "2-1", (7f, 7.5f) },
             { "2-2", (10f, 13f) },
             { "2-3", (12f, 13f) },
             { "3-1", (8f, 11.5f) },
-            { "3-2", (11f, 11f) },
+            { "3-2", (10.5f, 10.5f) },
             { "3-3", (15f, 15f) },
         };
 
         public static int GetSilver(float reference)
-            => Mathf.FloorToInt(reference * 30f / 100f);
+            => Mathf.CeilToInt(reference * 50f / 100f);
     }
 }
